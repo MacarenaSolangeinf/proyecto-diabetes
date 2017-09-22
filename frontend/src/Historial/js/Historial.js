1b56b6b6b6294f292700c6c8e8ad2c2ee734ffbb
Santiago Santana <santana.santiago@gmail.com>
2017-09-22 19:42:59 -0300

    Historial toma datos del server
    * Los datos del historial se encuentran en el path /data del server
    * Se añadió 'proxy' a package.json del frontend para enlazar react con el
    server y evitar CORS.
    * El componente App toma los datos del server en su estado y los pasa como
    propiedad al componente Historial

--- frontend/src/Historial/js/Historial.js
+++ frontend/src/Historial/js/Historial.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
-import {Row,Table,Button} from 'react-materialize';
+import { Row, Table, Button } from 'react-materialize';
 
 class Historial extends Component {
   render() {
     return (
-     <Row>
-     <Table bordered={true} responsive={true} centered={true}>
-              <thead>
-                <tr>
-                  <th data-field="fh">Fecha y hora</th>
-                  <th data-field="Medicion">Medición</th>
-                  <th data-field="Descripcion">Descripción</th>
-                  <th data-field="Insulina">Insulina</th>
-                  <th data-field="Medicación">Medicación</th>
-                  <th data-field="Accion">Acción</th>
-                </tr>
-              </thead>
-        <tbody>
-          <tr>
-            <td>06-05-17</td>
-            <td>123</td>
-            <td>Después de la merienda.</td>
-            <td>No</td>
-            <td>Si</td>
-             
-            <td><Button waves='light' className="red" ><b>Eliminar Medición</b></Button></td>
+      <Row>
+        <Table bordered={true} responsive={true} centered={true}>
+          <thead>
+            <tr>
+              <th data-field="fh">Fecha y hora</th>
+              <th data-field="Medicion">Medición</th>
+              <th data-field="Descripcion">Descripción</th>
+              <th data-field="Insulina">Insulina</th>
+              <th data-field="Medicación">Medicación</th>
+              <th data-field="Accion">Acción</th>
             </tr>
-      </tbody>
-      </Table>    
-            
-    </Row>   
+          </thead>
+          <tbody>
+              {this.props.data.map(dato =>
+               <tr key={dato.id}>
+                  <td>{dato.fecha}</td>
+                  <td>{dato.medicion}</td>
+                  <td>{dato.descripcion}</td>
+                  <td>{dato.insulina}</td>
+                  <td>{dato.medicacion}</td>
+                  <td><Button waves='light' className="red" ><b>Eliminar Medición</b></Button></td>
+                </tr>
+              )}
+          </tbody>
+        </Table>
+
+      </Row>
     );
   }
 }
