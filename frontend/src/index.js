69f96f8ac25018aac74f0d4cb844ffbd85822c71
Santiago Santana <santana.santiago@gmail.com>
2017-09-26 21:11:44 -0300

    MuiThemeProvider en index.js
    MuiThemeProvider permite dar estilo a material-ui, más información en:
    http://www.material-ui.com/#/get-started/usage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Medicion from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import App from './App';
+
+const muiTheme = getMuiTheme({
+});
+
+const MyApp = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(
-
- <Medicion/>,
+  <MyApp />,
   document.getElementById('root')
 );
