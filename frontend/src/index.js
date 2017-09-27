ff028c5c37344e2999559963e4633fa66f6d3084
Santiago Santana <santana.santiago@gmail.com>
2017-09-26 21:18:54 -0300

    Tabs de material-ui
    Se reemplazan los tabs de materialize por tabs de material-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { lightWhite, white, cyan400 } from 'material-ui/styles/colors';
 import App from './App';
 
 const muiTheme = getMuiTheme({
+  tabs: {
+    backgroundColor: cyan400,
+    selectedTextColor: white,
+    textColor: lightWhite
+  }
 });
 
 const MyApp = () => (
