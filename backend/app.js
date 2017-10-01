0d531e97026f53b93ec118b81cc93936c3a1b4ea
Macarena Solange Quiroga <msinf.quiroga@gmail.com>
2017-10-01 01:01:55 -0300

    Conexion con squelize y sqlite

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('./db/conecction.js');
 var index = require('./routes/index');
 var data = require('./routes/data');
 
---
+++ backend/db/conecction.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: '../../diabetes.db'
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Or you can simply use a connection uri
+//const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
